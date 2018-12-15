d7c61d904c315a956208f88497b2ff57776f0ab8
sinahk <sinahk1992@yahoo.com>
2018-12-15 10:39:37 +0330

    add icon logo

--- converterCToF/ViewController.swift
+++ converterCToF/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myTextfield: UITextField!
+    
+    @IBOutlet weak var myButton: UIButton!
+    
+    @IBAction func myButtonAction(_ sender: Any) {
+        let cls: Double = NSString(string: myTextfield.text!).doubleValue
+        let frn: Double = (1.8 * cls) + 32
+        myLabel.text = "\(cls) C is \(frn) F"
+        textFieldShouldReturn(myTextfield)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.myTextfield.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func textFieldShouldReturn (_ textField: UITextField) -> Bool  {
+        textField.resignFirstResponder()
+        return true
     }
 
 
